1f5c967c0cbaa994c323618b17b0190a62104106
vs356 <113789926+vs356@users.noreply.github.com>
2023-08-08 15:15:05 -0400

    Added measurement calculation file

---
+++ measurements.py
@@ -0,0 +1,66 @@
+# measurements in mm
+from math import sqrt
+
+mmA = 74
+mmC = 73
+mmD = 82
+mmE = 95
+mmF = 105
+mmG = 87
+mmH = 52
+mmI = 66
+mmJ = 67
+mmK = 84
+mmL = 155
+mmM = 165
+
+# dimensions in mm
+lenFinger = mmA
+widFinger = 0.5 * min(mmH, mmI, mmJ)
+dimPalmOne = 0.5 * mmC
+dimPalmTwo = 0.5 * mmK
+dimPalmThree = sqrt((0.8 * mmE)**2 - (mmC**2)) - dimPalmTwo
+dimPalmFour = sqrt(mmF**2 - mmG**2)
+dimPalmFive = mmG
+widWrist = 0.5 * mmL
+
+offTrap = 0.2 * mmE
+widTrap = sqrt((-mmE + dimPalmTwo + dimPalmOne + dimPalmThree) * 
+                  (mmE - dimPalmTwo + dimPalmOne + dimPalmThree) * 
+                  (mmE - dimPalmTwo - dimPalmOne + dimPalmThree) * 
+                  (mmE - dimPalmTwo + dimPalmOne - dimPalmThree)) / (2 * abs(mmE - dimPalmTwo))
+#heron's formula
+lenPalm = mmE
+widRect = widFinger + sqrt(dimPalmFour**2 - offTrap**2)
+widPalm = widTrap + widRect
+lenHand = mmA + mmE
+
+print("Sanity Check: \n\t Palm Length: ", lenPalm,
+      " mm \n\t Rectangle Width: ", widRect,
+      "mm \n\t Trapezoid Width: ", widTrap,
+      " mm \n\t Palm Width: ", round(widPalm, 4), 
+      " mm \n\t Hand Length: ", lenHand, " mm")
+
+#dimensions in chips
+cLenFinger = round(lenFinger * 0.6667)
+cWidFinger = round(widFinger * 0.75)
+cDimPalmOne = round(dimPalmOne * 0.6111)
+cDimPalmTwo = round(dimPalmTwo * 0.9655)
+cDimPalmThree = round(dimPalmThree * 0.5833)
+cDimPalmFour = round(dimPalmFour * 0.8125)
+cDimPalmFive = round(dimPalmFive * 0.7857)
+cWidWrist = round(widWrist * 0.75)
+
+cOffTrap = round(offTrap * 0.6667)
+cWidTrap = round(widTrap * 0.75)
+cLenPalm = round(lenPalm * 0.6667)
+cWidRect = round(widRect * 0.75)
+cWidPalm = round(widPalm * 0.75)
+cLenHand = round(lenHand * 0.6667)
+
+print("\n\nSanity Check 2: \n\t Palm Length: ", cLenPalm, 
+      " chips \n\t Palm Width: ", cWidPalm, 
+      " chips \n\t Hand Length: ", cLenHand, " chips")
+
+#coordinates of chips
+
